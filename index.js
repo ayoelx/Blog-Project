3360ab95e89615ec05075b128523a7a7810bf2de
Tommy <ayoelx@users.noreply.github.com>
2019-08-24 23:56:58 +0100

    Initial compose page creation and functionality

--- index.js
+++ index.js
@@ -17,24 +17,17 @@ let homeContent = "Multiple lines of text that form the lede, informing new read
 let contactContent = "Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.";
 let port = 4000;
 let options = {};
+let posts = [];
 
 
 app.get('/', (req, res) => {
   options = {
     homeContent: homeContent,
     pageTitle: "Tenner | Home",
-    titleContent: "Title of a longer featured blog post"
+    titleContent: "Title of a longer featured blog post",
+    posts: posts
   };
   res.render('home', options);
-});
-
-app.get('/contact', (req, res) => {
-  options = {
-    homeContent: contactContent,
-    pageTitle: "Contact Us | Tenner",
-    titleContent: "Contact Us"
-  }
-  res.render('contact', options);
 });
 
 app.get('/compose', (req, res) => {
@@ -45,5 +38,31 @@ app.get('/compose', (req, res) => {
 
   res.render('compose', options);
 });
+
+app.post('/compose', (req, res) => {
+  let title = req.body.title;
+  let content = req.body.content;
+  let category = req.body.category;
+
+  let day = new Date();
+  let now = day.toLocaleDateString('en-US', options);
+
+  options = {
+    month: 'long',
+    day: 'numeric'
+  }
+
+  let post = {
+    title: title,
+    content: content,
+    category: category,
+    date: now
+  };
+
+  posts.push(post);
+
+  res.redirect('/');
+});
+
 
 app.listen(port, () => console.log("Server listening on port " + port));
