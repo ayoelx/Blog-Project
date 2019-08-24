214e882a4ab9767087b0012be39eb5030043f494
Tommy <ayoelx@users.noreply.github.com>
2019-08-24 22:46:21 +0100

    Put compose dir

--- index.js
+++ index.js
@@ -13,8 +13,7 @@ app.set('view engine', 'ejs');
 
 
 //Pre-defined content
-let homeContent = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.";
-let aboutContent = "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio.";
+let homeContent = "Multiple lines of text that form the lede, informing new readers quickly and efficiently about what’s most interesting in this post’s contents.";
 let contactContent = "Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.";
 let port = 4000;
 let options = {};
@@ -24,7 +23,7 @@ app.get('/', (req, res) => {
   options = {
     homeContent: homeContent,
     pageTitle: "Tenner | Home",
-    titleContent: ""
+    titleContent: "Title of a longer featured blog post"
   };
   res.render('home', options);
 });
@@ -36,6 +35,15 @@ app.get('/contact', (req, res) => {
     titleContent: "Contact Us"
   }
   res.render('contact', options);
+});
+
+app.get('/compose', (req, res) => {
+  options = {
+    pageTitle: "Tenner | Compose",
+    titleContent: "Compose"
+  }
+
+  res.render('compose', options);
 });
 
 app.listen(port, () => console.log("Server listening on port " + port));
